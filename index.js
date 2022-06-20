ef5c21758e83c33c85643ba9f92ca270a975cf6d
sebas-777 <sebastiancast06@hotmail.com>
2022-06-19 21:56:32 -0500

    add pug

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express  from 'express' 
+import usuarioRoutes from './routes/usuarioRoutes.js'
 
 
 // crear la app
-
 const app = express()
 
+//Habilitar Pug 
+app.set('view engine', 'pug')
+app.set('views','./views')
+
 // Routing 
-
-
+app.use('/auth', usuarioRoutes)
 
 // definir un puerto y arrancar el proyecto 
-
 const port = 3000;
 app.listen(port, () => {
     console.log(`el servidor esta funcionado en el port ${port}`)
---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,17 @@
+import express  from 'express';	
+
+
+const router = express.Router();
+
+router.get('/login',(req, res) => {
+    res.render('auth/login',{
+        autenticado: false
+    })
+});
+
+
+
+
+
+
+export default router;
